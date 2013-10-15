af2a4c6a8e6068ba1281bba99d775143301c620c
janvt <janvt@me.com>
2013-10-15 20:01:36 +0200

    - index fix - jquery dependency - added pubsub to core

--- webMoves/public/js/main.js
+++ webMoves/public/js/main.js
@@ -1,3 +1,11 @@
+define.amd.jQuery = true;
+
+require.config({
+    paths: {
+        'jquery': 'libs/jquery.min'
+    }
+});
+
 require(['app'], function(app)
 {
     app.init({
--- webMoves/public/js/modules/core.js
+++ webMoves/public/js/modules/core.js
@@ -1,4 +1,36 @@
-define('modules/core', function()
+/**
+ * defines app core
+ * - acts as mediator for events & actions
+ *
+ * @author janvt
+ *
+ */
+
+define('modules/core', ['jquery'], function($)
 {
-    return {};
+    var pubsubhub = $({});
+
+    return {
+
+        // jvt: pubsub
+        subscribe: function()
+        {
+            pubsubhub.on.apply(pubsubhub, arguments);
+        }
+
+        , unsubscribe: function()
+        {
+            pubsubhub.off.apply(pubsubhub, arguments);
+        }
+
+        , subscribeOnce: function()
+        {
+            pubsubhub.one.apply(pubsubhub, arguments);
+        }
+
+        , publish: function()
+        {
+            pubsubhub.trigger.apply(pubsubhub, arguments);
+        }
+    };
 });
